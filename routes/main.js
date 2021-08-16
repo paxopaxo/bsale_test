acbc78a9a0e5f9908542537611d9b5cfe7e3597d
Max <paxipaxi2003@gmail.com>
2021-08-16 04:28:22 -0400

    fix favicon err

--- routes/main.js
+++ routes/main.js
@@ -7,6 +7,9 @@ const router = Router()
 router.get('/cats', (req,res) => {
     res.sendFile( path.join( __dirname ,'../public/cats.html' ) )
 })
+router.get('/favicon.ico', (req,res) => {
+    res.redirect('https://static.vecteezy.com/system/resources/previews/002/133/765/original/alcohol-bottle-rgb-color-icon-vector.jpg')
+})
 
 
 
